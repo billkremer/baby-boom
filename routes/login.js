592b31e473a6da9dddce57ede76ae74007e91df0
Bill Kremer <bkrems12@gmail.com>
2017-02-16 13:33:05 -0600

    added short route to get user id

--- routes/login.js
+++ routes/login.js
@@ -2,12 +2,20 @@ const router = require('express').Router();
 var passport = require('passport');
 
 router.post('/', passport.authenticate('local'), function(req, res){
+  console.log('gothere login.js?', req.user);
   res.sendStatus(200);
 });
 
 router.delete('/', function(req, res){
   req.logout();
   res.sendStatus(204);
+});
+
+
+router.get('/', passport.authenticate('local'), function(req, res){
+  console.log('gothere profile?', req.user);
+  res.status(200).send(req.user)
+  // res.sendStatus(200);
 });
 
 module.exports = router;
