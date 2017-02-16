0672048987659ddb320e4148c57878f6d0078761
Bill Kremer <bkrems12@gmail.com>
2017-02-16 13:38:03 -0600

    added methods to update the profile and get the initial data.

--- public/scripts/profile.controller.js
+++ public/scripts/profile.controller.js
@@ -1,8 +1,64 @@
 angular.module('BabyBoomApp').controller('ProfileController', function($http, $location){
 
+// console.log('result',result.user);
+var temp = {};
+
+var ctrl = this;
+
+ctrl.getProfileData = function () {
+  console.log("trying to get data");
+  $http.get('/profile').then(function(response) {
 
 
-  this.logout = function() {
+    console.log('gothere profilecontroller?', response);
+    ctrl.id = response.data.id;
+    ctrl.username = response.data.username;
+    ctrl.realName = response.data.user_fullname;
+    ctrl.babyName = response.data.user_baby_name;
+    ctrl.babyBirthday = new Date(response.data.user_baby_birthday); // comes from the db as a string? so needs to be a Date again...
+
+    }).catch(function(err){
+        console.log('Error getting data');
+      });
+
+  };
+// console.log(temp);
+
+
+ctrl.getProfileData();
+
+// console.log('this',this);
+
+
+ctrl.updateProfile = function () {
+
+  console.log(ctrl);
+
+  var objectToSend = {
+    id: ctrl.id,
+    username: ctrl.username,
+    realName: ctrl.realName,
+    babyName: ctrl.babyName,
+    babyBirthday: ctrl.babyBirthday,
+  }
+
+
+  $http.put('/profile', objectToSend);
+
+
+
+
+
+
+
+
+};
+
+
+
+
+
+  ctrl.logout = function() {
     $http.delete('/login').then(function(){
       console.log('Successfully logged out!');
       $location.path('/');
@@ -10,4 +66,6 @@ angular.module('BabyBoomApp').controller('ProfileController', function($http, $l
       console.log('Error logging out');
     });
   }
-});
+
+
+}); // closes controller
