6da327335663a31f84f1b984a77e534a8f9d5801
Bill Kremer <bkrems12@gmail.com>
2017-02-23 22:27:25 -0600

    cleaned up and rearranged table creation.

--- database-setup.sql
+++ database-setup.sql
@@ -2,60 +2,57 @@
 CREATE DATABASE baby_boom
 
 
+CREATE TABLE achievement_data ( -- the achievements the child has completed.
+	id SERIAL PRIMARY KEY,
+	userid INT, --or userID?
+	achievement_id INT, -- so that it doesn't always show - using a JOIN.
+  achievement_completed_text TEXT,
+  achievement_completed_date DATE,
+  achievement_completed_comment TEXT
+);
+
+
+CREATE TABLE achievements (
+	id SERIAL PRIMARY KEY,
+  achievement_age_months REAL, -- REAL is a pretty good number which has decimals.  needs decimals to compare to for month calculations.
+	achievement_text TEXT, -- the basic info about the achievement
+  achievement_placeholder TEXT --prompts the user to describe the experience
+);
+
+
+CREATE TABLE pictures (
+	id SERIAL PRIMARY KEY,
+	userid INT, -- equals picture owner
+	picture_url TEXT,
+	picture_comment TEXT,
+	picture_originalname TEXT
+);
+
+CREATE TABLE user_contacts (
+	id SERIAL PRIMARY KEY,
+	userid INT,
+	contactname TEXT,
+  contactemail TEXT
+);
+
 CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
 	username varchar(80) NOT NULL UNIQUE,
 	password varchar(256) NOT NULL,
 	user_fullname VARCHAR (100)
 	user_baby_name VARCHAR (80),
-	user_baby_birthday DATE -- timestamp?
-);
-
-
-CREATE TABLE achievements (
-	id SERIAL PRIMARY KEY,
-	achievement_text TEXT,
-  achievement_age_months REAL, -- number or text? // REAL is a pretty good number which has decimals.  needs decimals to compare to.
-  achievement_placeholder TEXT
-);
-
-
--- CREATE TABLE achievement_data (
--- 	id SERIAL PRIMARY KEY,
--- 	username varchar(80), --or userID?
--- 	achievement_id INT, -- so that it doesn't show - using a JOIN.
---   achievement_completed TEXT,
---   achievement_completed_date DATE, --? timestamp?
---   achievement_completed_comment TEXT
--- );
-
-CREATE TABLE achievement_data (
-	id SERIAL PRIMARY KEY,
-	userid INT, --or userID?
-	achievement_id INT, -- so that it doesn't show - using a JOIN.
-  achievement_completed_text TEXT,
-  achievement_completed_date DATE, --? timestamp?
-  achievement_completed_comment TEXT
+	user_baby_birthday DATE
 );
 
 
 
-CREATE TABLE user_contacts (
+CREATE TABLE email_history (
 	id SERIAL PRIMARY KEY,
 	userid INT,
-	contactName TEXT,
-  contactEmail TEXT
-);
 
 
--- CREATE TABLE email_history (
--- 	id SERIAL PRIMARY KEY,
---
--- );
 
 
-CREATE TABLE pictures (
-	id SERIAL PRIMARY KEY,
-	userid INT, -- equals picture owner
-	picture_url TEXT
+
+
 );
